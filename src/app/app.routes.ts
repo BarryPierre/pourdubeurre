26e908bb1f987d683c2caef7e1a089db1e764013
Pierre Barry <barrypierre@gmail.com>
2024-06-11 16:47:53 +0200

    azert

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,8 @@
 import { Routes } from '@angular/router'
 import { PourdubeurreBisComponent } from './pourdubeurre-bis/pourdubeurre-bis.component'
-import { PourdubeurreComponent } from './pourdubeurre/pourdubeurre.component'
 
 export const routes: Routes = [
   
-  {
-    path: 'pourdubeurre',
-    component:PourdubeurreComponent
-  },
   {
     path: 'pourdubeurrebis',
     component:PourdubeurreBisComponent
